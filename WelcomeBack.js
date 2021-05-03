cae189190a9037039c7e7b600616773e3b209c77
Camila B. Carlocchia <ccarlocchia@gmail.com>
2021-05-03 16:44:00 -0300

    Adding ReactJs

---
+++ WelcomeBack.js
@@ -0,0 +1,19 @@
+class WelcomeBack extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            appVersion: ''
+        }
+    }
+
+    render(){
+        return (
+            <>
+                <h2>Hello Friend! Welcome Back.</h2>
+                <button>Download</button>
+            </>
+            
+        )
+    }
+}
